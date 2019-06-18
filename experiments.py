24f0226b8fbb3d0ca12b4346fa01c8e7f5f690f3
triko <trikotrako1@gmail.com>
2019-06-18 07:50:20 +0300

    a

--- experiments.py
+++ experiments.py
@@ -68,7 +68,7 @@ def run_experiment(run_name, out_dir='./results', seed=None,
 
     dl_train = torch.utils.data.DataLoader(ds_train, bs_train)
     dl_test = torch.utils.data.DataLoader(ds_test, bs_test)
-    fit_res = trainer.fit(dl_train, dl_test, epochs, early_stopping=early_stopping, print_every=2)
+    fit_res = trainer.fit(dl_train, dl_test, epochs, early_stopping=early_stopping)
     # ========================
 
     save_experiment(run_name, out_dir, cfg, fit_res)
